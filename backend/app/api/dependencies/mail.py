17a92e5ac77c48624d7c9f1cf48186d99cc92b81
realdegrees <18383355+realdegrees@users.noreply.github.com>
2025-11-07 03:16:57 +0100

    fetch postgresql from repo, fix linter rror

--- backend/app/api/dependencies/mail.py
+++ backend/app/api/dependencies/mail.py
@@ -5,7 +5,6 @@ from email.mime.text import MIMEText
 from typing import Annotated, Any
 
 from bs4 import BeautifulSoup
-from itsdangerous import URLSafeTimedSerializer
 from core.config import (
     EMAIL_PRESIGN_SECRET,
     JINJA_ENV,
@@ -17,8 +16,8 @@ from core.config import (
 )
 from core.logger import get_logger
 from fastapi import Depends, HTTPException
+from itsdangerous import URLSafeTimedSerializer
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
 from util.api_router import APIRouter
 
 app_logger = get_logger("app")
