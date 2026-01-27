3fe3bdac65193b1fa5c021d80f99654b2a586202
realdegrees <18383355+realdegrees@users.noreply.github.com>
2026-01-27 14:00:35 +0100

    add security headers

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -12,6 +12,23 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
+		csp: {
+			mode: 'auto',
+			directives: {
+				'default-src': ['self'],
+				'script-src': ['self'],
+				'style-src': ['self', 'unsafe-inline'],
+				'connect-src': process.env.NODE_ENV === 'development'
+					? ['self', 'wss:', 'ws:', 'http:', 'https:', 'blob:']
+					: ['self', 'wss:', 'https:', 'blob:'],
+				'img-src': ['self', 'data:', 'blob:'],
+				'worker-src': ['self', 'blob:'],
+				'object-src': ['none'],
+				'base-uri': ['self'],
+				'form-action': ['self'],
+				'frame-ancestors': ['none']
+			}
+		},
 		alias: {
 			'$api/*': './src/api/*',
 			'$types/*': './src/types/*',
